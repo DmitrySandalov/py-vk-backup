2e703024dd18c16136b9f49c6ef5be90f82c1bc3
Dmitry Sandalov <dmitry@sandalov.org>
2016-11-06 04:05:43 +0300

    fix TypeError print format

--- vkbackup.py
+++ vkbackup.py
@@ -59,7 +59,7 @@ def download_album(group, alb):
 
 
 def download_album_to_dir(group, album, size, directory):
-    print(directory + ' [album_id: ' + album + ']')
+    print(directory, ' [ album_id:', album, ']')
     if not os.path.exists(directory):
         os.makedirs(directory)
     counter = 1
